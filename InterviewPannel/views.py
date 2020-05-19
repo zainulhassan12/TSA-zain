f7bcf24f6e1efde8effcd2a4bb598c426f1e2bcc
Zain ul Hassan <zaniimirzaa@gmail.com>
2020-05-20 02:38:45 +0500

    "fixed list problem of ajax request"

--- InterviewPannel/views.py
+++ InterviewPannel/views.py
@@ -1,15 +1,18 @@
-from django.contrib import messages
-from django.contrib.admin.views.decorators import staff_member_required
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_exempt
 import json
 
+from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+
 from .Forms import QuizForm, answers, questions
+
+
 # Create your views here.
 @staff_member_required
 def interhome(request):
     return render(request, "index1.html")
+
 
 @staff_member_required
 def QuizView(request):
@@ -27,42 +30,59 @@ def QuizView(request):
     }
     return render(request, "index.html", context)
 
+
 @csrf_exempt
 @staff_member_required
 def ans(request):
+    global i
     if request.is_ajax() and request.method == 'POST':
-        form = questions()
+
 
         question = request.POST.get('Question')
         option = request.POST.getlist('Options[]')
+        # obj = json.loads(request.POST.get(''))
+        obj1 = json.loads(request.body)
+        question = obj1['Question']
+        options = obj1['Options']
+        i = 0
+        for x in options:
+            op = options[i]
+            opt = op['Option']
+            IsTrue = op['IsTrue']
+            # print(op)
+            print(opt)
+            print(IsTrue)
+            i = i+1
+        # print(options)
 
-        # print(request.__dict__)
-        options = {'Options': request.json['Options']}
-        print(options)
-        # options1 = request.__dict__
-        # print(request.__dict__)
-        # print(request.__list__)
-        # form.Question = question
-        # form.save()
-        # data = json.loads(request.POST.get('Data', ''))
-        # print(data)
-        # print(options1)
         print(question)
-        return HttpResponse("success njkhhkh")
+
+        # print(obj)
+
+    # print(request.__dict__)
+    # options = {'Options': request.POST.getlist('Options[]')}
+    # print(options)
+    # options1 = request.__dict__
+    # print(request.__dict__)
+    # print(request.__list__)
+    # form.Question = question
+    # form.save()
+    # data = json.loads(request.POST.get('Data', ''))
+    # print(data)
+    # print(options1)
+    # print(question)
     return render(request, "MasterDetail.html")
 
 
-
-
-    # form = answers(request.POST or None)
-    # if form.is_valid():
-    #
-    #     for answer in form:
-    #         form.answer = form.cleaned_data('answer')
-    #         form.is_correct = form.cleaned_data('is_correct')
-    #         print(answer)
-    #         print(form)
-    #     form.save()
+# form = answers(request.POST or None)
+# if form.is_valid():
+#
+#     for answer in form:
+#         form.answer = form.cleaned_data('answer')
+#         form.is_correct = form.cleaned_data('is_correct')
+#         print(answer)
+#         print(form)
+#     form.save()
 
 @staff_member_required
 def QuestionAdd(request):
